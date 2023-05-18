b9bf74825aaac17c48b4e6c3dd2b2533019804a3
InayahUmmuArdillah <inayahummuardillah@gmail.com>
2023-05-18 22:17:46 +0630

    di while juga nim sudah ada

--- 143_SINGLELINKEDLIST/143_SINGLELINKEDLIST.cpp
+++ 143_SINGLELINKEDLIST/143_SINGLELINKEDLIST.cpp
@@ -22,12 +22,31 @@ void addNode() {
 
 	if (START == NULL || nim <= START->noMhs) {
 		if (START != NULL && nim == START->noMhs)
-	 {
+		{
 			cout << "NIM sudah ada" << endl;
 			return;
-	}
+		}
 
 		nodeBaru->next = START;
 		START = nodeBaru;
 		return;
+	}
+
+	Node* previous = START;
+	Node* current = START;
+
+	while ((current != NULL) && (nim >= current->noMhs))
+	{
+		if (nim == current->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+		previous = current;
+		current = current->next;
+	}
+
+	nodeBaru->next = current;
+	previous-> next = nodeBaru;
 }
+
