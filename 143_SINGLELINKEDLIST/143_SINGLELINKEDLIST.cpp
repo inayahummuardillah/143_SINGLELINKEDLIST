328eee310dd9714fd9fc8069fe62406c18e00b47
InayahUmmuArdillah <inayahummuardillah@gmail.com>
2023-05-18 22:58:12 +0630

    memasukan nim dan menampilkan nim

--- 143_SINGLELINKEDLIST/143_SINGLELINKEDLIST.cpp
+++ 143_SINGLELINKEDLIST/143_SINGLELINKEDLIST.cpp
@@ -158,25 +158,4 @@ int main() {
 					system("pause");
 					system("cls");
 				}
-				else
-					cout << "Data tidak ditemukan" << endl;
-				break;
-			case 3:
-			
-				break;
-			case 4:
-				searchData();
-				break;
-			case 5:
-				break;
-			default:
-				cout << "Piliham tidak ada " << endl;
-				break;
-			}
-		}
-		catch (exception e)
-		{
-			cout << "Terjadi Kesalahan" << endl;
- 		}
-	} while (pilihan != 5);
 }
